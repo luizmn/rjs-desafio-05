757d3b5f191684bc6e480316e7797121a5349688
luizmn <luizmn@outlook.com>
2021-08-25 20:39:50 +0100

    updating styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
+import Head from "next/head";
+import Image from "next/image";
+
+import styles from "./header.module.scss";
+
 export default function Header() {
-  // TODO
+  return (
+    <>
+      <Head>
+        <title>spacetraveling</title>
+      </Head>
+      <header className={styles.logo}>
+        <Image src="/logo.svg" alt="logo" width={238} height={25} />
+      </header>
+    </>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,62 @@
-import { GetStaticProps } from 'next';
+import { GetStaticProps } from "next";
+import Header from "../components/Header";
 
-import { getPrismicClient } from '../services/prismic';
+import { getPrismicClient } from "../services/prismic";
 
-import commonStyles from '../styles/common.module.scss';
-import styles from './home.module.scss';
+import commonStyles from "../styles/common.module.scss";
+import styles from "./home.module.scss";
 
-interface Post {
-  uid?: string;
-  first_publication_date: string | null;
-  data: {
-    title: string;
-    subtitle: string;
-    author: string;
-  };
-}
+import Image from "next/image";
 
-interface PostPagination {
-  next_page: string;
-  results: Post[];
-}
-
-interface HomeProps {
-  postsPagination: PostPagination;
-}
-
-// export default function Home() {
-//   // TODO
+// interface Post {
+//   uid?: string;
+//   first_publication_date: string | null;
+//   data: {
+//     title: string;
+//     subtitle: string;
+//     author: string;
+//   };
 // }
+
+// interface PostPagination {
+//   next_page: string;
+//   results: Post[];
+// }
+
+// interface HomeProps {
+//   postsPagination: PostPagination;
+// }
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <div className={styles.container}>
+        <div className={styles.post}>
+          <h1 className={styles.title}>Como utilizar Hooks </h1>
+          <p className={styles.subtitle}>
+            Pensando em sincronização em vez de ciclos de vida.
+          </p>
+          <div className={styles.info}>
+            <span className={styles.createdAt}>25 Ago 2021</span>
+            <span className={styles.author}>Joseph Climber</span>
+          </div>
+        </div>
+        <div className={styles.post}>
+          <h1 className={styles.title}>Criando um app CRA do zero </h1>
+          <p className={styles.subtitle}>
+            Tudo sobre como criar a sua própria aplicação com o React
+          </p>
+          <div className={styles.info}>
+            <span className={styles.createdAt}>25 Ago 2021</span>
+            <span className={styles.author}>Joseph Climber</span>
+          </div>
+        </div>
+        <button className={styles.loadButton}>Carregar mais posts</button>
+      </div>
+    </>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
