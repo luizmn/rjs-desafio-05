ded1a71c76329750ac0534d9b16fc4d71eb1f58f
luizmn <luizmn@outlook.com>
2021-08-26 12:15:51 +0100

    updated styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import { GetStaticProps } from "next";
-import Header from "../components/Header";
+import Image from "next/image";
+
+import { FiCalendar, FiUser } from "react-icons/fi";
 
 import { getPrismicClient } from "../services/prismic";
 
 import commonStyles from "../styles/common.module.scss";
 import styles from "./home.module.scss";
 
-import Image from "next/image";
+import Header from "../components/Header";
 
 // interface Post {
 //   uid?: string;
@@ -30,29 +32,43 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <Header />
       <div className={styles.container}>
+        <Header />
         <div className={styles.post}>
           <h1 className={styles.title}>Como utilizar Hooks </h1>
           <p className={styles.subtitle}>
             Pensando em sincronização em vez de ciclos de vida.
           </p>
           <div className={styles.info}>
-            <span className={styles.createdAt}>25 Ago 2021</span>
-            <span className={styles.author}>Joseph Climber</span>
+            <span>
+              <FiCalendar />
+            </span>
+            <span>25 Ago 2021</span>
+            <span>
+              <FiUser />
+            </span>
+            <span>Joseph Climber</span>
           </div>
         </div>
         <div className={styles.post}>
           <h1 className={styles.title}>Criando um app CRA do zero </h1>
           <p className={styles.subtitle}>
-            Tudo sobre como criar a sua própria aplicação com o React
+            Tudo sobre como criar a sua própria aplicação utilizando Create
+            React App
           </p>
           <div className={styles.info}>
-            <span className={styles.createdAt}>25 Ago 2021</span>
-            <span className={styles.author}>Joseph Climber</span>
+            <span>
+              <FiCalendar />
+            </span>
+            <span>25 Ago 2021</span>
+            <span>
+              <FiUser />
+            </span>
+            <span>Joseph Climber</span>
           </div>
         </div>
-        <button className={styles.loadButton}>Carregar mais posts</button>
+
+        <span className={styles.loadMore}>Carregar mais posts</span>
       </div>
     </>
   )
